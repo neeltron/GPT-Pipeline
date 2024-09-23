7e9348750b425acc1a7df08be3b0a0e3039cf292
Neel Adwani <nprosofficial@gmail.com>
2024-09-22 20:14:36 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 21:48:09 2024
+
+@author: CAR Lab
+"""
+
+import openai
+import matlab.engine
+
+eng = matlab.engine.connect_matlab()
+
+openai.api_key = 'API_KEY_HERE'
+messages = [ {"role": "system", "content": 
+              "You are a intelligent assistant."} ]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.chat.completions.create(
+            model="gpt-4o", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+    eng.workspace['matlab_var'] = reply
+    
+eng.quit()
+    
